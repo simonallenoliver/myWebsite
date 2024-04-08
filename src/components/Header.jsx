8c9c2cf3603827fcbc974d6ebb6c03becb3b3f7e
SimonAllenOliver <simonallenoliver@gmail.com>
2024-04-08 13:20:51 -0500

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+      <div className='headerBox'>
+        <div>menu contact github linkedin</div>
+      </div>
+
+    </>
+  )
+}
+
+export default Header
